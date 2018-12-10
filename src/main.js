45feb16525f0c448bff9a0470c2499f936d28ce6
philip-assessment-bitlab <45767461+philip-assessment-bitlab@users.noreply.github.com>
2018-12-10 18:10:13 +0100

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -1,105 +1,118 @@
-(function () {
-  var beginAC = 80,
-    endAC = 320,
-    beginB = 80,
-    endB = 320;
+var begin = 80,
+	end = 320,
+	pathD = document.getElementById('pathD'),
+	pathE = document.getElementById('pathE'),
+	pathF = document.getElementById('pathF'),
+	segmentD = new Segment(pathD, begin, end),
+	segmentE = new Segment(pathE, begin, end),
+	segmentF = new Segment(pathF, begin, end),
+	wrapper = document.getElementById('menu-icon-wrapper'),
+	trigger = document.getElementById('menu-icon-trigger'),
+	toCloseIcon = true,
+	nav = document.getElementById('menu'),
+	menuItem1 = document.getElementsByTagName("li")[0],
+	menuItem2 = document.getElementsByTagName("li")[1];
 
-  function inAC(s) {
-    s.draw('80% - 240', '80%', 0.3, {
-      delay: 0.1,
-      callback: function () {
-        inAC2(s)
-      }
-    });
-  }
+function createOverlayLeft() {
+	let newOverlay = document.createElement("div");
+	newOverlay.setAttribute("id", "leftOverlay");
+	let newContent = document.createTextNode("Overlay");
+	newOverlay.appendChild(newContent);
+	let currentDiv = document.getElementById("div1");
+	document.body.insertBefore(newOverlay, currentDiv);
+	newOverlay.style.display = "none";
+	newOverlay.style.background = "blue";
+	newOverlay.style.position = "fixed";
+	newOverlay.style.width = "50%";
+	newOverlay.style.height = "100%";
+	newOverlay.style.top = 0;
+	newOverlay.style.left = 0;
+	newOverlay.style.right = 0;
+	newOverlay.style.bottom = 0;
+	newOverlay.style.cursor = "pointer";
+	newOverlay.addEventListener("click", hideOverlays);
+}
 
-  function inAC2(s) {
-    s.draw('100% - 545', '100% - 305', 0.6, {
-      easing: ease.ease('elastic-out', 1, 0.3)
-    });
-  }
+function createOverlayRight() {
+	let newOverlay = document.createElement("div");
+	newOverlay.setAttribute("id", "rightOverlay");
+	let newContent = document.createTextNode("Overlay");
+	newOverlay.appendChild(newContent);
+	let currentDiv = document.getElementById("div1");
+	document.body.insertBefore(newOverlay, currentDiv);
+	newOverlay.style.display = "none";
+	newOverlay.style.background = "red";
+	newOverlay.style.position = "fixed";
+	newOverlay.style.width = "50%";
+	newOverlay.style.height = "100%";
+	newOverlay.style.top = 0;
+	newOverlay.style.left = "50%";
+	newOverlay.style.right = 0;
+	newOverlay.style.bottom = 0;
+	newOverlay.style.cursor = "pointer";
+	newOverlay.addEventListener("click", hideOverlays);
+}
 
-  function inB(s) {
-    s.draw(beginB - 60, endB + 60, 0.1, {
-      callback: function () {
-        inB2(s)
-      }
-    });
-  }
+createOverlayLeft();
+createOverlayRight();
 
-  function inB2(s) {
-    s.draw(beginB + 120, endB - 120, 0.3, {
-      easing: ease.ease('bounce-out', 1, 0.3)
-    });
-  }
+menuItem1.addEventListener("click", displayOverlays);
+menuItem2.addEventListener("click", displayOverlays);
 
-  function outAC(s) {
-    s.draw('90% - 240', '90%', 0.1, {
-      easing: ease.ease('elastic-in', 1, 0.3),
-      callback: function () {
-        outAC2(s)
-      }
-    });
-  }
+function displayOverlays() {
+	let leftOverlay = document.getElementById("leftOverlay");
+	let rightOverlay = document.getElementById("rightOverlay");
+	leftOverlay.style.display = "block";
+	rightOverlay.style.display = "block";
+}
 
-  function outAC2(s) {
-    s.draw('20% - 240', '20%', 0.3, {
-      callback: function () {
-        outAC3(s)
-      }
-    });
-  }
+function hideOverlays() {
+	let leftOverlay = document.getElementById("leftOverlay");
+	let rightOverlay = document.getElementById("rightOverlay");
+	leftOverlay.style.display = "none";
+	rightOverlay.style.display = "none";
+}
 
-  function outAC3(s) {
-    s.draw(beginAC, endAC, 0.7, {
-      easing: ease.ease('elastic-out', 1, 0.3)
-    });
-  }
+var btn = document.getElementById('rnmSearch');
+btn.onclick = function () {
+  var name = document.getElementById("rnmName").value;
 
-  function outB(s) {
-    s.draw(beginB, endB, 0.7, {
-      delay: 0.1,
-      easing: ease.ease('elastic-out', 2, 0.4)
-    });
-  }
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      var res = JSON.parse(this.responseText);
 
-  function addScale(m) {
-    m.className = 'scaled';
-  }
+      document.getElementById('names').innerHTML = '';
+      for (var i = 0; i < res.results.length; i++) {
+        document.getElementById('names').innerHTML += res.results[i].name + "<br>";
 
-  function removeScale(m) {
-    m.className = '';
-  }
-
-  var pathD = document.getElementById('pathD'),
-    pathE = document.getElementById('pathE'),
-    pathF = document.getElementById('pathF'),
-    segmentD = new Segment(pathD, beginAC, endAC),
-    segmentE = new Segment(pathE, beginB, endB),
-    segmentF = new Segment(pathF, beginAC, endAC),
-    wrapper = document.getElementById('menu-icon-wrapper'),
-    trigger = document.getElementById('menu-icon-trigger'),
-    toCloseIcon = true,
-    nav = document.getElementById('menu');
-
-  wrapper.style.visibility = 'visible';
-
-  trigger.onclick = function () {
-    addScale(wrapper);
-    if (toCloseIcon) {
-      inAC(segmentD);
-      inB(segmentE);
-      inAC(segmentF);
-      nav.style.display = 'block';
-    } else {
-      outAC(segmentD);
-      outB(segmentE);
-      outAC(segmentF);
-      nav.style.display = 'none';
+        if (i === 9) {
+          break;
+        }
+      } 
     }
-    toCloseIcon = !toCloseIcon;
-    setTimeout(function () {
-      removeScale(wrapper)
-    }, 450);
   };
-})();
+  xhttp.open("GET", "https://rickandmortyapi.com/api/character/?name=" + name, true);
+  xhttp.send();
+};
+
+function addScale(m) {
+	m.className = 'scaled';
+}
+
+function removeScale(m) {
+	m.className = '';
+}
+
+trigger.onclick = function () {
+	addScale(wrapper);
+	setTimeout(function () {
+		removeScale(wrapper)
+	}, 150);
+
+	if (nav.style.display === 'block') {
+		nav.style.display = 'none';
+	} else {
+		nav.style.display = 'block';
+	}
+};
